d3edfda0bba3167b15171de40e6322f6a77560fd
AgarwalDhruv359 <agarwaldhruva359@gmail.com>
2025-05-13 13:52:39 +0530

    Add files via upload

---
+++ wp.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pickle
+
+
+st.title("Weather Prediction App..")
+pn=st.number_input("Enter Precipitation")
+maxt=st.number_input("Enter Maximum Temperature")
+mint=st.number_input("Enter Minimum Temperature")
+wind=st.number_input("Enter Wind Speed")
+button=st.button("Predict!")
+if button:
+    lr=pickle.load(open("wp.pkl","rb"))
+    res=lr.predict([[pn,maxt,mint,wind]])[0]
+    st.markdown(f"Today Weather Situation: {res}")
